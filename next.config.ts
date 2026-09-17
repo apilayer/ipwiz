183df4215358eb0493f28a1565fbb5e5108cf5bc
vaibhav208 <vaibhav.20555@gmail.com>
2026-09-17 10:53:57 +0530

    based path added and pipeline fixed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix: "/devtools/ipwiz",
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/devtools/ipwiz',
+  assetPrefix: '/devtools/ipwiz',
+  images: { unoptimized: true },
 };
-
-export default nextConfig;
